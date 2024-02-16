970883d3d28d2fe30c3a277a0874de9f564d3c52
Talha Naaeem <talha.naeem97@gmail.com>
2024-02-16 18:45:18 +0500

    keyboard issue

--- components/auth/signUpForm/signUpForm.tsx
+++ components/auth/signUpForm/signUpForm.tsx
@@ -136,7 +136,7 @@ const SignUpForm = () => {
     }
 
     return (
-        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined} style={{ flex: 1 }}>
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <View>
                     <View>
