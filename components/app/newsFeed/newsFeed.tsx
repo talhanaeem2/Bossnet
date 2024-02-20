b4bbd481fff605d263ee0e59931ab243868498eb
Talha Naaeem <talha.naeem97@gmail.com>
2024-02-20 23:40:01 +0500

    fixes

--- components/app/newsFeed/newsFeed.tsx
+++ components/app/newsFeed/newsFeed.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, ActivityIndicator, FlatList } from "react-native"
+import { View, StyleSheet, FlatList } from "react-native"
 import { memo, useCallback, useEffect, useState } from "react"
 import axios from "axios"
 
