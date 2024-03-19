fdce71094aee251f2178f0d02e9070e3d3f04969
Talha Naaeem <talha.naeem97@gmail.com>
2024-03-19 13:25:43 +0500

    remove

--- components/app/postDotMenu/postDotMenu.tsx
+++ components/app/postDotMenu/postDotMenu.tsx
@@ -1,5 +1,5 @@
 import { View, StyleSheet, TouchableOpacity } from "react-native"
-import { memo, useCallback, useEffect } from "react";
+import { memo, useEffect } from "react";
 
 import TextRegular from "../textComponent/textRegular/textRegular";
 
@@ -9,8 +9,6 @@ import debounce from "../../../constants/debounceFunction";
 
 import PostDotMenuProps from "./interfaces/postDotMenuProps";
 import { RPH, RPW } from "../../../constants/utils";
-import Apis from "../../../constants/apis";
-import axios from "axios";
 
 const PostDotMenu = (props: PostDotMenuProps) => {
     const { activeIndex, onMenuPress, index, isMenuVisible, setIsMenuVisible, postId } = props
